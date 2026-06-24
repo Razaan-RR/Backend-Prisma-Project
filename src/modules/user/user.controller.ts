1c96810bf0b23522123c951cc2987530a6d57808
Razaan Reza <razaanreza0705@gmail.com>
2026-06-24 21:18:40 +0600

    user login

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,34 +1,24 @@
-import { Request, Response } from 'express'
-import { prisma } from '../../lib/prisma'
-import bcrypt from 'bcryptjs'
-import config from '../../config'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import httpStatus from 'http-status'
 import { userService } from './user.service'
+import { catchAsync } from '../../utils/catchAsync'
+import { sendResponse } from '../../utils/sendResponse'
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
+const registerUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body
 
     const user = await userService.registerUserIntoDB(payload)
 
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
       success: true,
       statusCode: httpStatus.CREATED,
-      message: 'User Created Successfully',
-      data: {
-        user,
-      },
+      message: "User Registered successfully",
+      data:{user}
     })
-  } catch (error) {
-    console.log(error)
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-      message: 'Failed to register user',
-      error: (error as Error).message,
-    })
-  }
-}
+
+  },
+)
 
 export const userController = {
   registerUser,
